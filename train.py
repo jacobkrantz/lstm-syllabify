ddc4dcda93dcd57c83806b540d606048b1b49ee8
jacobkrantz <jkrantz@zagmail.gonzaga.edu>
2019-04-05 11:05:06 -0700

    Add LSTM size and embedding size to updated params

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
-# This script trains the BiLSTM-CRF architecture for syllabification using
-# the CELEX English dataset.
+# This script trains the BiLSTM-CRF architecture for syllabification.
+
 import os
 import logging
 import sys
@@ -75,63 +75,70 @@ def create_directory(size):
         # Make sure the directory doesn't already exist.
         os.mkdir(PATH + "/" + str(size))
 
-# parameters 'use_cnn', 'cnn_filter_size', and 'cnn_filter_length' don't do anything yet. CNN development is WIP.
+# run_lst: [name, cnn layers, cnn number filters, cnn filter size, cnn max pool size, dropout, lstm size, embedding size]
+# run_lst = [
+#     ['1-test-layer',        6,  40, 3, 2, 0.25, 100, 100],
+#     ['2-test-filter',       2, 100, 3, 2, 0.25, 100, 100],
+#     ['3-test-layer-filter', 6, 100, 3, 2, 0.25, 100, 100],
+#     ['4-test-filter-size',  2,  40, 2, 2, 0.25, 100, 100]
+# ]
 
-
-# Max run for batch sizes
-# Organized by name, cnn layers, cnn number filters, cnn filter size, cnn max pool size, dropout*
-# run0 = [2,40,3,2,0.25]
-# run1 = ['1-test-layer',6,40,3,2,0.25]
-# run2 = ['2-test-filter', 2,100,3,2,0.25]
-# run3 = ['3-test-layer-filter', 6,100,3,2,0.25]
-# run4 = ['4-test-filter-size', 2,40,2,2,0.25]
+# run_lst = [
+#     [ '9', 2, 100, 2,    4, 0.25, 100, 100],
+#     ['10', 2, 200, 3,    2, 0.25, 100, 100],
+#     ['11', 2, 200, 2,    2, 0.25, 100, 100],
+#     ['12', 1,  40, 3,    2, 0.25, 100, 100],
+#     ['13', 1, 100, 3,    2, 0.25, 100, 100],
+#     ['14', 1, 200, 3,    2, 0.25, 100, 100],
+#     ['15', 2, 200, 2,    4, 0.25, 100, 100],
+#     ['16', 2, 100, 2, None, 0.25, 100, 100],
+#     ['17', 2, 200, 2, None, 0.25, 100, 100],
+#     ['18', 2, 100, 3, None, 0.25, 100, 100]
+# ]
 
 run_lst = [
-    [ '9', 2, 100, 2,    4, 0.25],
-    ['10', 2, 200, 3,    2, 0.25],
-    ['11', 2, 200, 2,    2, 0.25],
-    ['12', 1,  40, 3,    2, 0.25],
-    ['13', 1, 100, 3,    2, 0.25],
-    ['14', 1, 200, 3,    2, 0.25],
-    ['15', 2, 200, 2,    4, 0.25],
-    ['16', 2, 100, 2, None, 0.25],
-    ['17', 2, 200, 2, None, 0.25],
-    ['18', 2, 100, 3, None, 0.25]
+    ['19', 1, 200, 3, 2, 0.25, 200, 100],
+    ['20', 1, 200, 3, 2, 0.25, 100, 200],
+    ['21', 1, 200, 3, 2, 0.25, 200, 200],
+    ['22', 2, 200, 3, 2, 0.25, 200, 100],
+    ['23', 2, 200, 3, 2, 0.25, 100, 200],
+    ['24', 2, 200, 3, 2, 0.25, 200, 200]
 ]
+
 for run in run_lst:
-        create_directory(run[0])
-        print("Entering run: ", run[0])
+    create_directory(run[0])
+    print("Entering run: ", run[0])
 
-        for iteration in range(0,21): # Run one extra test for an outlier to be removed.
-                file_path = PATH + "/" + str(run[0]) + "/" + str(iteration) + '.csv'
-                print(run[1],run[2],run[3],run[4])
-                params_to_update = {
-    # LSTM related
-    'which_rnn': 'LSTM', # either 'LSTM' or 'GRU'
-    'lstm_size': 100,
-    'dropout': 0.25, # (0.25, 0.25), # tuple dropout is for recurrent dropout and cannot work with GPU computation.
-    # CNN related
-    'use_cnn': True,
-    'cnn_layers': run[1],
-    'cnn_num_filters': run[2],
-    'cnn_filter_size': run[3],
-    'cnn_max_pool_size': run[4], # if None or False, do not use MaxPooling
+    for iteration in range(0,21): # Run one extra test for an outlier to be removed.
+        file_path = PATH + "/" + str(run[0]) + "/" + str(iteration) + '.csv'
+        print("Run with updated parameters: ", run)
+        params_to_update = {
+            # LSTM related
+            'which_rnn': 'LSTM', # either 'LSTM' or 'GRU'
+            'lstm_size': run[6],
+            'dropout': 0.25, # (0.25, 0.25), # tuple dropout is for recurrent dropout and cannot work with GPU computation.
+            # CNN related
+            'use_cnn': True,
+            'cnn_layers': run[1],
+            'cnn_num_filters': run[2],
+            'cnn_filter_size': run[3],
+            'cnn_max_pool_size': run[4], # if None or False, do not use MaxPooling
 
-    # CRF related
-    'classifier': 'crf', # either 'softmax', 'kc-crf' (from keras-contrib) or 'crf' (by Philipp Gross).
-    'crf_activation': 'linear', # Only for kc-crf. Possible values: 'linear' (default), 'relu', 'tanh', 'softmax', others. See Keras Activations.
+            # CRF related
+            'classifier': 'crf', # either 'softmax', 'kc-crf' (from keras-contrib) or 'crf' (by Philipp Gross).
+            'crf_activation': 'linear', # Only for kc-crf. Possible values: 'linear' (default), 'relu', 'tanh', 'softmax', others. See Keras Activations.
 
-    # general params
-    'mini_batch_size': 64,
-    'using_gpu': True,
-    'embedding_size': 100,
-    'early_stopping': 10
-                }
+            # general params
+            'mini_batch_size': 64,
+            'using_gpu': True,
+            'embedding_size': run[7],
+            'early_stopping': 10
+        }
 
-                model = BiLSTM(params_to_update)
-                model.set_vocab_size(vocab_size, n_class_labels, word_length, mappings)
-                model.set_dataset(datasets, data)
-                model.store_results(file_path) # Path to store performance scores for dev / test
-                model.model_save_path = "models/[ModelName]_[DevScore]_[TestScore]_[Epoch].h5" # Path to store models
-                model.fit(epochs = 120)
-                os.system("rm /home/ubuntu/gulp/lstm-syllabify/models/* -rf")
+        model = BiLSTM(params_to_update)
+        model.set_vocab_size(vocab_size, n_class_labels, word_length, mappings)
+        model.set_dataset(datasets, data)
+        model.store_results(file_path) # Path to store performance scores for dev / test
+        model.model_save_path = "models/[ModelName]_[DevScore]_[TestScore]_[Epoch].h5" # Path to store models
+        model.fit(epochs = 120)
+        os.system("rm /home/ubuntu/gulp/lstm-syllabify/models/* -rf")
